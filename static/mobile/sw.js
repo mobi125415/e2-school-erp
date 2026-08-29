5115dc4ef53973c215ae68bbd7bd611f01c6b010
mobi125415 <malikmmq4125415@gmail.com>
2026-08-29 11:08:38 +0500

    Delete static directory

--- static/mobile/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='e2-mobile-v3';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
